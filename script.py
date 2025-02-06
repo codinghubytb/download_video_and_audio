115e383d4a5cba534f64e9faf68deb8334abe1e1
CodingHub <95412136+codinghubytb@users.noreply.github.com>
2025-02-06 17:30:51 +0100

    Add Script General

---
+++ script.py
@@ -0,0 +1,31 @@
+import yt_dlp
+import argparse
+import os
+
+def download_instagram_video(url, titre, extension, cookies_file=None):
+    output_directory = ""
+    os.makedirs(output_directory, exist_ok=True)
+
+    ydl_opts = {
+        'outtmpl': os.path.join(output_directory, f"{titre}.%(ext)s"),
+        'format': 'bestvideo+bestaudio/best' if extension == "video" else 'bestaudio/best',
+    }
+
+    if cookies_file:
+        ydl_opts['cookiefile'] = cookies_file
+
+    with yt_dlp.YoutubeDL(ydl_opts) as ydl:
+        ydl.download([url])
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="Telecharge une video.")
+    parser.add_argument("url", help="URL")
+    parser.add_argument("title", help="Titre du fichier (sans extension)")
+    parser.add_argument("extension", choices=["video", "audio"], help="Extension du fichier (video ou audio)")
+    parser.add_argument("cookies", help="Chemin vers le fichier de cookies", default=None)
+    
+    
+    args = parser.parse_args()
+
+    download_instagram_video(args.url, args.title, args.extension, args.cookies)
+    print(f"Download Finished : {args.extension.upper()} !")
